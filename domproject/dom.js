187fad473e82cf1b7231aab1e83dc467c5c27ec6
Vidit-Yadav <vidityadav.2k@gmail.com>
2022-12-22 21:21:43 +0530

    DOM-task-3

---
+++ domproject/dom.js
@@ -0,0 +1,15 @@
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.all);
+
+
+//var headerTitle=document.getElementById("header-title");
+//console.log(headerTitle);
+var header=document.getElementById("main-header");
+//header.style.borderBottom="solid 3px #000"
+var subtitle=document.getElementsByClassName("title");
+console.log(subtitle);
+//subtitle.style.fontWeight="bold";
+subtitle[0].style.fontWeight='bold';
+subtitle[0].style.color='green';
