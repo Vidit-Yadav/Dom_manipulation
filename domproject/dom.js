bde41eb37f678edb917d33f683527802ca986353
Vidit-Yadav <70445606+Vidit-Yadav@users.noreply.github.com>
2022-12-23 19:01:59 +0530

    dom-task-4

--- domproject/dom.js
+++ domproject/dom.js
@@ -9,7 +9,27 @@
 var header=document.getElementById("main-header");
 //header.style.borderBottom="solid 3px #000"
 var subtitle=document.getElementsByClassName("title");
-console.log(subtitle);
+//console.log(subtitle);
 //subtitle.style.fontWeight="bold";
 subtitle[0].style.fontWeight='bold';
 subtitle[0].style.color='green';
+subtitle[1].style.color='yellow';
+var item=document.getElementsByClassName('list-group-item')
+console.log(item)
+for (var i=0; i<item.length;i++){
+    item[i].style.fontWeight='bold';
+}
+item[2].style.backgroundColor='green';
+//
+
+var li=document.getElementsByTagName("li");
+//console.log(subtitle);
+//subtitle.style.fontWeight="bold";
+// li[0].style.fontWeight='bold';
+// li[0].style.color='green';
+// li[1].style.color='yellow';
+
+for (var i=0; i<li.length;i++){
+    li[i].style.fontWeight='bold';
+    li[i].style.backgroundColor='#f2f2f2';
+}
